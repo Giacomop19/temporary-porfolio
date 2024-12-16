4390f6d0c0c952a2cd95d613811441800e3dae61
Giacomo Pumapillo <giacomo.pumapillo@outlook.it>
2024-12-16 17:39:33 +0100

    fixing mobile view on experience section-2

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -4,35 +4,71 @@ export default function Experience(){
             <div className="flex items-center gap-8 mb-12" style={{flexDirection:"row-reverse"}}>
                 <div className="w-full h-[1px] bg-zinc-700"></div>
                     <h2>
-                        <div className="relative overflow-hidden w-fit">
+                        <div className="relative w-fit">
                             <div style={{opacity: 1, transform: "none"}}><span className="text-3xl md:text-5xl font-black text-end">Experience<span className="text-indigo-500">.</span></span></div>
-                                <div className="absolute bottom-1 left-0 right-0 top-1 z-20 bg-indigo-500" style={{left: "100%"}}></div>
-                        </div>
+                                </div>
                     </h2>
             </div>
-            {/* --- EXPERIENCE 1 -- */}
+            {/* --- EXPERIENCE 0 -- */}
             <div className="mb-6 border-b pb-6 border-zinc-700">
-                <div className="flex items-center justify-between mb-2">
-                    <div className="relative overflow-hidden w-fit">
-                        <div style={{opacity: 1, transform: "none"}}><span className="font-bold text-xl">Relewant</span></div>
-                        <div className="absolute bottom-1 left-0 right-0 top-1 z-20 bg-indigo-500" style={{left: "100%"}}></div>
+                <div className="flex items-center justify-between gap-10 mb-2">
+                    <div className="relative w-fit">
+                        <div style={{opacity: 1, transform: "none"}}><span className="font-bold text-xl">Avaloq</span></div>
                     </div>
-                    <div className="relative overflow-hidden w-fit">
-                        <div style={{opacity: 1, transform: "none"}}><span className="font-bold">11/2023 - Present</span></div>
-                        <div className="absolute bottom-1 left-0 right-0 top-1 z-20 bg-indigo-500" style={{left: "100%"}}></div>
+                    <div className="relative w-fit">
+                        <div style={{opacity: 1, transform: "none"}}><span className="font-bold  ">01/2025 - present</span></div>
                     </div>
                 </div>
                 <div className="flex items-center justify-between mb-4">
-                    <div className="relative overflow-hidden w-fit">
+                    <div className="relative w-fit">
                         <div style={{opacity: 1, transform: "none"}}><span className="text-indigo-300 font-bold">Software Engineer</span></div>
-                            <div className="absolute bottom-1 left-0 right-0 top-1 z-20 bg-indigo-500" style={{left: "100%"}}></div>
                     </div>
-                    <div className="relative overflow-hidden w-fit">
+                    <div className="relative  w-fit">
                         <div style={{opacity: 1, transform: "none"}}><span className="font-bold">Switzerland</span></div>
-                        <div className="absolute bottom-1 left-0 right-0 top-1 z-20 bg-indigo-500" style={{left: "100%"}}></div>
                     </div>
                 </div>
-                <div className="relative overflow-hidden w-fit">
+                <div className="relative w-fit">
+                    <div style={{opacity: 1, transform: "none"}}>
+                        <p className="mb-6 text-zinc-300 leading-relaxed">I develop main digital services of banking in Switzerland.
+                        </p>
+                    </div>
+                </div>
+                <div className="relative w-fit">
+                    <div style={{opacity: 1, transform: "none"}}>
+                        <div className="flex flex-wrap gap-2">
+                            <span className="text-xs px-2 py-1 rounded bg-zinc-700">Angular</span>
+                            <span className="text-xs px-2 py-1 rounded bg-zinc-700">Java</span>
+                            <span className="text-xs px-2 py-1 rounded bg-zinc-700">Kotlin</span>
+                            <span className="text-xs px-2 py-1 rounded bg-zinc-700">SpringBoot</span>
+                            <span className="text-xs px-2 py-1 rounded bg-zinc-700">OracleDB</span>
+                            <span className="text-xs px-2 py-1 rounded bg-zinc-700">OpenApi</span>
+                            <span className="text-xs px-2 py-1 rounded bg-zinc-700">Maven</span>
+                            <span className="text-xs px-2 py-1 rounded bg-zinc-700">Docker</span>
+                            <span className="text-xs px-2 py-1 rounded bg-zinc-700">Android Studio</span>
+                        </div>
+                    </div>
+                    <div className="absolute bottom-1 left-0 right-0 top-1 z-20 bg-indigo-500" style={{left: "100%"}}></div>
+                </div>
+            </div>
+            {/* --- EXPERIENCE 1 -- */}
+            <div className="mb-6 border-b pb-6 border-zinc-700">
+                <div className="flex items-center justify-between mb-2 gap-10">
+                    <div className="relative  w-fit">
+                        <div style={{opacity: 1, transform: "none"}}><span className="font-bold text-xl">Relewant</span></div>
+                    </div>
+                    <div className="relative w-fit">
+                        <div style={{opacity: 1, transform: "none"}}><span className="font-bold ">07/2023 - 12/2024</span></div>
+                    </div>
+                </div>
+                <div className="flex items-center justify-between mb-4">
+                    <div className="relative w-fit">
+                        <div style={{opacity: 1, transform: "none"}}><span className="text-indigo-300 font-bold">Software Engineer</span></div>
+                        </div>
+                    <div className="relative w-fit">
+                        <div style={{opacity: 1, transform: "none"}}><span className="font-bold">Switzerland</span></div>
+                    </div>
+                </div>
+                <div className="relative w-fit">
                     <div style={{opacity: 1, transform: "none"}}>
                         <p className="mb-6 text-zinc-300 leading-relaxed">I help develop a software for the italian regions that helps employment centres manage the hiring of people with disabilities. It also checks that companies are compliant according to current regulations.
                             I do actually build API and user interface.
@@ -40,7 +76,7 @@ export default function Experience(){
                     </div>
                     <div className="absolute bottom-1 left-0 right-0 top-1 z-20 bg-indigo-500" style={{left: "100%"}}></div>
                 </div>
-                <div className="relative overflow-hidden w-fit">
+                <div className="relative w-fit">
                     <div style={{opacity: 1, transform: "none"}}>
                         <div className="flex flex-wrap gap-2">
                             <span className="text-xs px-2 py-1 rounded bg-zinc-700">Angular</span>
@@ -57,34 +93,30 @@ export default function Experience(){
             </div>
             {/* --- EXPERIENCE 2 -- */}
             <div className="mb-6 border-b pb-6 border-zinc-700">
-                <div className="flex items-center justify-between mb-2">
-                    <div className="relative overflow-hidden w-fit">
+                <div className="flex items-center justify-between mb-2 gap-10">
+                    <div className="relative w-fit">
                         <div style={{opacity: 1, transform: "none"}}><span className="font-bold text-xl">Alten Italy</span></div>
-                        <div className="absolute bottom-1 left-0 right-0 top-1 z-20 bg-indigo-500" style={{left: "100%"}}></div>
                     </div>
-                    <div className="relative overflow-hidden w-fit">
-                        <div style={{opacity: 1, transform: "none"}}><span className="font-bold">01/2023 - 11/2023</span></div>
-                        <div className="absolute bottom-1 left-0 right-0 top-1 z-20 bg-indigo-500" style={{left: "100%"}}></div>
+                    <div className="relative w-fit">
+                        <div style={{opacity: 1, transform: "none"}}><span className="font-bold ">01/2022 - 06/2023</span></div>
                     </div>
                 </div>
                 <div className="flex items-center justify-between mb-4">
-                    <div className="relative overflow-hidden w-fit">
+                    <div className="relative w-fit">
                         <div style={{opacity: 1, transform: "none"}}><span className="text-indigo-300 font-bold">Software Developer</span></div>
-                            <div className="absolute bottom-1 left-0 right-0 top-1 z-20 bg-indigo-500" style={{left: "100%"}}></div>
-                    </div>
-                    <div className="relative overflow-hidden w-fit">
+                        </div>
+                    <div className="relative w-fit">
                         <div style={{opacity: 1, transform: "none"}}><span className="font-bold">Italy</span></div>
-                        <div className="absolute bottom-1 left-0 right-0 top-1 z-20 bg-indigo-500" style={{left: "100%"}}></div>
                     </div>
                 </div>
-                <div className="relative overflow-hidden w-fit">
+                <div className="relative w-fit">
                     <div style={{opacity: 1, transform: "none"}}>
                         <p className="mb-6 text-zinc-300 leading-relaxed">I had the opportunity to work exclusively as extern supplier for the bank Intesa Sanpaolo, within the Internet Banking service, the application used by customers for manage their banking account and services.
                         </p>
                     </div>
                     <div className="absolute bottom-1 left-0 right-0 top-1 z-20 bg-indigo-500" style={{left: "100%"}}></div>
                 </div>
-                <div className="relative overflow-hidden w-fit">
+                <div className="relative w-fit">
                     <div style={{opacity: 1, transform: "none"}}>
                         <div className="flex flex-wrap gap-2">
                             <span className="text-xs px-2 py-1 rounded bg-zinc-700">Angular</span>
@@ -102,34 +134,30 @@ export default function Experience(){
             </div>
             {/* --- EXPERIENCE 3 -- */}
             <div className="mb-6 border-b pb-6 border-zinc-700">
-                <div className="flex items-center justify-between mb-2">
-                    <div className="relative overflow-hidden w-fit">
+                <div className="flex items-center justify-between mb-2 gap-10">
+                    <div className="relative w-fit">
                         <div style={{opacity: 1, transform: "none"}}><span className="font-bold text-xl">Integris</span></div>
-                        <div className="absolute bottom-1 left-0 right-0 top-1 z-20 bg-indigo-500" style={{left: "100%"}}></div>
                     </div>
-                    <div className="relative overflow-hidden w-fit">
-                        <div style={{opacity: 1, transform: "none"}}><span className="font-bold">01/2022 - 12/2022</span></div>
-                        <div className="absolute bottom-1 left-0 right-0 top-1 z-20 bg-indigo-500" style={{left: "100%"}}></div>
+                    <div className="relative w-fit">
+                        <div style={{opacity: 1, transform: "none"}}><span className="font-bold ">01/2021 - 12/2021</span></div>
                     </div>
                 </div>
                 <div className="flex items-center justify-between mb-4">
-                    <div className="relative overflow-hidden w-fit">
-                        <div style={{opacity: 1, transform: "none"}}><span className="text-indigo-300 font-bold">Front-end Developer</span></div>
-                            <div className="absolute bottom-1 left-0 right-0 top-1 z-20 bg-indigo-500" style={{left: "100%"}}></div>
-                    </div>
-                    <div className="relative overflow-hidden w-fit">
+                    <div className="relative w-fit">
+                        <div style={{opacity: 1, transform: "none"}}><span className="text-indigo-300 font-bold">Software Developer</span></div>
+                        </div>
+                    <div className="relative w-fit">
                         <div style={{opacity: 1, transform: "none"}}><span className="font-bold">Italy</span></div>
-                        <div className="absolute bottom-1 left-0 right-0 top-1 z-20 bg-indigo-500" style={{left: "100%"}}></div>
                     </div>
                 </div>
-                <div className="relative overflow-hidden w-fit">
+                <div className="relative w-fit">
                     <div style={{opacity: 1, transform: "none"}}>
                         <p className="mb-6 text-zinc-300 leading-relaxed">During this period, I had the opportunity to work as an external contractor in the software development team of Bridgestone, a web-based application used to store and manage customer services information.
                         </p>
                     </div>
                     <div className="absolute bottom-1 left-0 right-0 top-1 z-20 bg-indigo-500" style={{left: "100%"}}></div>
                 </div>
-                <div className="relative overflow-hidden w-fit">
+                <div className="relative w-fit">
                     <div style={{opacity: 1, transform: "none"}}>
                         <div className="flex flex-wrap gap-2">
                             <span className="text-xs px-2 py-1 rounded bg-zinc-700">Angular</span>
@@ -143,34 +171,30 @@ export default function Experience(){
             </div>
             {/* --- EXPERIENCE 4 -- */}
             <div className="mb-6 border-b pb-6 border-zinc-700">
-                <div className="flex items-center justify-between mb-2">
-                    <div className="relative overflow-hidden w-fit">
+                <div className="flex items-center justify-between mb-2 gap-10">
+                    <div className="relative w-fit">
                         <div style={{opacity: 1, transform: "none"}}><span className="font-bold text-xl">Trace One</span></div>
-                        <div className="absolute bottom-1 left-0 right-0 top-1 z-20 bg-indigo-500" style={{left: "100%"}}></div>
                     </div>
-                    <div className="relative overflow-hidden w-fit">
-                        <div style={{opacity: 1, transform: "none"}}><span className="font-bold">01/2021 - 12/2021</span></div>
-                        <div className="absolute bottom-1 left-0 right-0 top-1 z-20 bg-indigo-500" style={{left: "100%"}}></div>
+                    <div className="relative w-fit">
+                        <div style={{opacity: 1, transform: "none"}}><span className="font-bold ">01/2020 - 12/2020</span></div>
                     </div>
                 </div>
                 <div className="flex items-center justify-between mb-4">
-                    <div className="relative overflow-hidden w-fit">
+                    <div className="relative w-fit">
                         <div style={{opacity: 1, transform: "none"}}><span className="text-indigo-300 font-bold">Jr Software Developer</span></div>
-                            <div className="absolute bottom-1 left-0 right-0 top-1 z-20 bg-indigo-500" style={{left: "100%"}}></div>
-                    </div>
-                    <div className="relative overflow-hidden w-fit">
+                        </div>
+                    <div className="relative w-fit">
                         <div style={{opacity: 1, transform: "none"}}><span className="font-bold">Italy</span></div>
-                        <div className="absolute bottom-1 left-0 right-0 top-1 z-20 bg-indigo-500" style={{left: "100%"}}></div>
                     </div>
                 </div>
-                <div className="relative overflow-hidden w-fit">
+                <div className="relative w-fit">
                     <div style={{opacity: 1, transform: "none"}}>
                         <p className="mb-6 text-zinc-300 leading-relaxed">I developed a Software application aimed to perform the business core. Main development using C# and .NET framework.
                         </p>
                     </div>
                     <div className="absolute bottom-1 left-0 right-0 top-1 z-20 bg-indigo-500" style={{left: "100%"}}></div>
                 </div>
-                <div className="relative overflow-hidden w-fit">
+                <div className="relative w-fit">
                     <div style={{opacity: 1, transform: "none"}}>
                         <div className="flex flex-wrap gap-2">
                             <span className="text-xs px-2 py-1 rounded bg-zinc-700">ASP .NET MVC</span>
